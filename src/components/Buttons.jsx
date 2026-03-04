a5f0722fba47814d30ed896bcb0a5da25fc07552
A97Campos <andrewleandro97@gmail.com>
2026-03-04 09:32:57 -0300

    Delete Buttons.jsx

--- src/components/Buttons.jsx
+++
@@ -1,9 +0,0 @@
-function Button(props) {
-    return (
-        <button className="bg-slate-400 p-2 rounded-md text-white" onClick={props.onClick}>
-            {props.children}
-        </button>
-    )
-}
-
-export default Button
